4462a400493be7f6b4a5fc402cbc63adf4c8f145
Pedro Tauzen <pedrotauzen@gmail.com>
2023-12-11 20:39:17 +0000

    SignUpFragment layout created

---
+++ app/src/main/java/pt/ipbeja/todoapp/ui/fragments/SignUpFragment.kt
@@ -0,0 +1,28 @@
+package pt.ipbeja.todoapp.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import pt.ipbeja.todoapp.R
+import pt.ipbeja.todoapp.databinding.FragmentSingUpBinding
+
+class SignUpFragment : Fragment() {
+
+    private lateinit var binding: FragmentSingUpBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ) = FragmentSingUpBinding.inflate(inflater).also {
+        binding = it
+    }.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.alreadyHaveAccountTxt.setOnClickListener {
+            findNavController()
+                .navigate(R.id.logInFragment)
+        }
+    }
+}

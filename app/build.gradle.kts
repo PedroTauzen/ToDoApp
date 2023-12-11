1f42033a40461b4408e254a9579aff22a646b048
Pedro Tauzen <pedrotauzen@gmail.com>
2023-12-11 19:57:44 +0000

    Added material design to the fragment_log_in.xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
 
     implementation("androidx.room:room-common:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
+
     // Navigation
     val navVersion = "2.7.5"
     implementation ("androidx.navigation:navigation-fragment-ktx:$navVersion")
@@ -51,6 +52,9 @@ dependencies {
 
     // Fragment
     implementation ("androidx.fragment:fragment-ktx:1.6.2")
+
+    // Google Material Design
+    implementation ("com.google.android.material:material:1.10.0")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
